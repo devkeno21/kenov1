e7c4d9f7dced392df43de7b1be7c80370d653a95
dawit-getahun-mengistu <112875081+dawit-getahun-mengistu@users.noreply.github.com>
2023-09-08 16:15:58 +0300

    modified:   index.ts

--- index.ts
+++ index.ts
@@ -1,29 +1,22 @@
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { connect } from "@planetscale/database";
-import { env } from "~/env.mjs";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import { migrate } from 'drizzle-orm/mysql2/migrator';
+import { env } from "~/env.mjs"; 
 
-const dbHost = env.DATABASE_HOST;
-const dbUsername = env.DATABASE_USERNAME;
-const dbPassword = env.DATABASE_PASSWORD;
-// create the connection
-const connection = connect({
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+
+
+const dbHost = env.DATABASE_HOST
+const dbUsername = env.DATABASE_USERNAME
+const dbPassword = env.DATABASE_PASSWORD
+
+const connection = mysql.createPool({
+  database: "kenov1",
   host: dbHost,
-  username: dbUsername,
-  password: dbPassword,
+  user: dbUsername,
+  password: dbPassword
 });
+ 
+const db = drizzle(connection);
 
-export const db = drizzle(connection);
-// import { drizzle } from "drizzle-orm/mysql2";
-// import mysql from "mysql2/promise";
-// import { migrate } from 'drizzle-orm/mysql2/migrator';
-
-// /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
-
-// const connection = mysql.createPool({
-//   database: "kenov1",
-
-// });
-
-// const db = drizzle(connection);
-
-// await migrate(db, { migrationsFolder: './drizzle' });
+await migrate(db, { migrationsFolder: './drizzle' });
