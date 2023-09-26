aa3f35cd9368173a4ffb0344a2e46104e2674207
mubarekmuhiyie <babyjoedoe1234@gmail.com>
2023-09-26 11:14:37 +0300

    fix(slicer): make slicer typesafe

--- src/store/timerSlice.tsx
+++ src/store/timerSlice.tsx
@@ -1,16 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+// Define the state type
+interface TimerState {
+  minutes: number;
+  seconds: number;
+}
+
+const initialState: TimerState = {
+  minutes: 0,
+  seconds: 0,
+};
 
 const timerSlice = createSlice({
   name: 'timer',
-  initialState: {
-    minutes: 0,
-    seconds: 0,
-  },
+  initialState,
   reducers: {
-    setMinutes: (state, action) => {
+    setMinutes: (state, action: PayloadAction<number>) => {
       state.minutes = action.payload;
     },
-    setSeconds: (state, action) => {
+    setSeconds: (state, action: PayloadAction<number>) => {
       state.seconds = action.payload;
     },
   },
@@ -18,3 +27,8 @@ const timerSlice = createSlice({
 
 export const { setMinutes, setSeconds } = timerSlice.actions;
 export default timerSlice.reducer;
+
+// Define a RootState type to use in your Redux store
+export type RootState = {
+  timer: TimerState;
+};
